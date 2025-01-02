7adf4e91bd7ed76477b38283ce4c673a5720336d
yamanoku <0910yama@gmail.com>
2025-01-02 20:04:42 +0900

    月区切りのページを生成する

--- index.ts
+++ index.ts
@@ -21,12 +21,13 @@ type DataType = {
   }[];
 };
 
-const OUTBOX_JSON_DATA = (await Bun.file(
+const OUTBOX_JSON_DATA: DataType = (await Bun.file(
   "./archive/outbox.json"
-).json()) as DataType;
+).json());
 
 const extractContent = (data: DataType) => {
-  return data.orderedItems.map(
+  const monthData: { [key: string]: string } = {};
+  data.orderedItems.forEach(
     (item) => {
       let content = item.object.content;
       if (!content) {
@@ -44,7 +45,11 @@ const extractContent = (data: DataType) => {
         );
       }
       const url = item.object.url;
-      return `
+      const month = new Date(published).getMonth() + 1;
+      if (!monthData[month]) {
+        monthData[month] = "";
+      }
+      monthData[month] += `
         <article>
           <time datetime="${published}">
             <a href="${url}" target="_blank">
@@ -62,15 +67,60 @@ const extractContent = (data: DataType) => {
       `;
     }
   );
+  return monthData;
 };
 
 const contentArray = extractContent(OUTBOX_JSON_DATA);
 
-// HTMLコンテンツを生成する
-const htmlContent = `
-<!DOCTYPE html><html lang="ja"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>2024 yamanoku's ActivityPub Contents</title><link rel="stylesheet" href="//cdn.skypack.dev/yama-normalize" /></head><body><main><h1>2024 yamanoku's ActivityPub Contents</h1>${contentArray.join("\n")}</main><footer style="text-align: center"><p>© Copyright ${new Date().getFullYear()}, Okuto Oyama</p><p>Source :<a href="https://github.com/yamanoku/2024-activityPub-contents/">yamanoku/2024-activityPub-contents</a></p></footer></body></html>`;
+const indexHTMLContent = `
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>2024 yamanoku's ActivityPub Contents</title>
+  <link rel="stylesheet" href="//cdn.skypack.dev/yama-normalize" />
+</head>
+<body>
+  <main>
+    <h1>2024 yamanoku's ActivityPub Contents</h1>
+    <ul>
+        ${Object.keys(contentArray)
+        .map(
+            (month) =>
+            `<li><a href="month-${month.padStart(2, "0")}.html">${month}月</a></li>`
+        )
+        .join("")}
+    </ul>
+  </main>
+  <footer style="text-align: center"><p>© Copyright ${new Date().getFullYear()}, Okuto Oyama</p><p>Source :<a href="https://github.com/yamanoku/2024-activityPub-contents/">yamanoku/2024-activityPub-contents</a></p></footer>
+</body>
+</html>
+`;
 
-// HTMLファイルに書き出す
-await Bun.write("build/index.html", htmlContent);
+const monthHTMLContent = (month: string) => `
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>2024 yamanoku's ActivityPub Contents - ${month} Month</title>
+  <link rel="stylesheet" href="//cdn.skypack.dev/yama-normalize" />
+</head>
+<body>
+  <main>
+    <h1>2024 yamanoku's ActivityPub Contents - ${month} Month</h1>
+    ${contentArray[month]}
+  </main>
+  <footer style="text-align: center"><p>© Copyright ${new Date().getFullYear()}, Okuto Oyama</p><p>Source :<a href="https://github.com/yamanoku/2024-activityPub-contents/">yamanoku/2024-activityPub-contents</a></p></footer>
+</body>
+</html>
+`;
+
+await Bun.write("build/index.html", indexHTMLContent);
+
+for (const month in contentArray) {
+  await Bun.write(`build/month-${month.padStart(2, "0")}.html`, monthHTMLContent(month));
+}
 
 await $`cp -r archive/media_attachments build/media_attachments`;
